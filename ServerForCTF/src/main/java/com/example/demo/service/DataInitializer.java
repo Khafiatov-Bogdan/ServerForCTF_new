88f99f7fb76830f84499ca47290937f4ebec1a3e
Khafiatov-Bogdan <karlapingus20@gmail.com>
2025-12-12 10:23:33 +0400

    Реалтзовал добаление баллов за лабы

--- ServerForCTF/src/main/java/com/example/demo/service/DataInitializer.java
+++ ServerForCTF/src/main/java/com/example/demo/service/DataInitializer.java
@@ -172,10 +172,10 @@ public class DataInitializer implements CommandLineRunner  {
                 {"POMOGITE10", "9"},//1
                 {"POMOGITE15", "16"},//1
                 {"POMOGITE20", "19"},//1
-                {"Nikita5", "5"},//1
-                {"Nikita9", "9"},//1
-                {"Nikita16", "16"},//1
-                {"Nikita19", "19"},//1
+                {"SPARKI5", "5"},//1
+                {"SPARKI9", "9"},//1
+                {"SPARKI16", "16"},//1
+                {"SPARKI19", "19"},//1
                 {"OPANA", "5"}//1
 
         };
